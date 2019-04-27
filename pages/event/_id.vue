dbfc587519b185ea2774993b72c114b1adc9c6b4
João Vicente <joao@javabeats.com.br>
2019-04-26 23:39:07 -0300

    Continuando com o treinamento

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Evento #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Evento #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- moved this over from index.vue
+          content: 'Tudo que você precisa saber sobre o evento #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Criar um Evento</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Criação de Evento',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- moved this over from index.vue
+          content: 'Crie um novo evento na sua visinhança'
+        }
+      ]
+    }
+  }
+}
+</script>
